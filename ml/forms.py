e4ae47749ba003618cad6d25d4c43347f0614c5d
TeamUs <temapavsok@gmail.com>
2024-04-09 16:12:54 +0300

    Правки с переводом моделей

--- ml/forms.py
+++ ml/forms.py
@@ -1,11 +1,10 @@
-# titanic_predictor/forms.py
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 class ModelChoiceForm(forms.Form):
     model_choices = [
-        ('gradient_boosting', 'Градиентный бустинг'),
-        ('polynomial_regression', 'Полиномиальная регрессия'),
-        ('rnn', 'Рекуррентная нейронная сеть'),
+        ('gradient_boosting', _('Градиентный бустинг')),
+        ('polynomial_regression', _('Полиномиальная регрессия')),
+        ('rnn', _('Рекуррентная нейронная сеть')),
     ]
-    model = forms.ChoiceField(choices=model_choices,label="")
-
+    model = forms.ChoiceField(choices=model_choices, label="")
